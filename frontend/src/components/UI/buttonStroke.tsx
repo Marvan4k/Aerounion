3338fcced8495b1833be071d99e01bbf58e2c846
Marvan4k <romanmarvanov@gmail.com>
2026-06-28 18:05:32 +0300

    Added translation to the main page

--- frontend/src/components/UI/buttonStroke.tsx
+++ frontend/src/components/UI/buttonStroke.tsx
@@ -8,7 +8,7 @@ interface ButtonStrokeProps {
 export default function ButtonStroke({ ref, textButton, className, ...rest }: ButtonStrokeProps){
     return (
         <a href={ref} className={`
-            inline-block px-16 py-3.5 border-2 border-white uppercase font-bold transition-all duration-300 
+            inline-block px-16 py-3.5 border-2 border-white uppercase font-bold transition-all duration-300 tracking-[10%]
             hover:bg-white hover:text-[#0a0a0a] 
             ${className}`} 
             {...rest}
